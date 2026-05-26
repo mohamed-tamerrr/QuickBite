cfc6a36e0086c3aa22de22b04c46b5731c093b1c
Mohamed Tamer <mohamed.tamerrr79@gmail.com>
2026-05-27 01:03:47 +0300

    authetication done and profile all done and auto login

--- lib/features/auth/view/profile_view.dart
+++ lib/features/auth/view/profile_view.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
+import 'package:hungry/core/network/api_exceptions.dart';
+import 'package:hungry/features/auth/data/auth_repo.dart';
+import 'package:hungry/features/auth/data/user_model.dart';
+import 'package:hungry/shared/custom_snack.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 import '../../../core/constants/app_colors.dart';
 import '../../../shared/custom_text.dart';
 
@@ -15,12 +20,37 @@ class _ProfileViewState extends State<ProfileView> {
   TextEditingController name = TextEditingController();
   TextEditingController email = TextEditingController();
   TextEditingController address = TextEditingController();
+  TextEditingController visa = TextEditingController();
+  UserModel? userModel;
+  final AuthRepo _authRepo = AuthRepo();
+
+  Future<void> getProfileData() async {
+    try {
+      final user = await _authRepo.getProfileData();
+      setState(() {
+        userModel = user;
+      });
+    } catch (e) {
+      String error = 'Error in profile';
+      if (e is Failure) {
+        error = e.errorMassage;
+      }
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(customSnack(error));
+    }
+  }
 
   @override
   void initState() {
-    name.text = 'Mohamed Tamer';
-    email.text = 'mohamed.tamer@example.com';
-    address.text = '123 Main Street, City, Country';
+    getProfileData().then((value) {
+      name.text = userModel?.name ?? 'Mohamed Tamer';
+      email.text =
+          userModel?.email ?? 'mohamed.tamer@example.com';
+      address.text =
+          userModel?.address ?? '123 Main Street, City, Country';
+    });
+
     super.initState();
   }
 
@@ -34,156 +64,197 @@ class _ProfileViewState extends State<ProfileView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.primary,
-      appBar: AppBar(
-        backgroundColor: AppColors.primary,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () => Navigator.of(context).pop(),
-          icon: const Icon(
-            Icons.arrow_back_ios_new_rounded,
-            color: Colors.white,
+    return RefreshIndicator(
+      color: Colors.red,
+      onRefresh: () async {
+        await getProfileData();
+      },
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          backgroundColor: AppColors.primary,
+          appBar: AppBar(
+            backgroundColor: AppColors.primary,
+            elevation: 0,
+            leading: IconButton(
+              onPressed: () => Navigator.of(context).pop(),
+              icon: const Icon(
+                Icons.arrow_back_ios_new_rounded,
+                color: Colors.white,
+              ),
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                  vertical: 8,
+                ),
+                child: SvgPicture.asset(
+                  'assets/settings.svg',
+                  width: 20,
+                ),
+              ),
+            ],
           ),
-        ),
-        actions: [
-          Padding(
+          body: Padding(
             padding: const EdgeInsets.symmetric(
-              horizontal: 8.0,
+              horizontal: 12.0,
               vertical: 8,
             ),
-            child: SvgPicture.asset(
-              'assets/settings.svg',
-              width: 20,
+            child: SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              child: Skeletonizer(
+                enabled: userModel == null,
+                child: Column(
+                  children: [
+                    Center(
+                      child: Container(
+                        height: 120,
+                        width: 120,
+                        decoration: BoxDecoration(
+                          image:
+                              userModel?.image != null &&
+                                  userModel!.image!.isNotEmpty
+                              ? DecorationImage(
+                                  image: NetworkImage(
+                                    userModel!.image!,
+                                  ),
+                                )
+                              : null,
+                          // borderRadius: BorderRadius.circular(12),
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 2,
+                          ),
+                          shape: BoxShape.circle,
+                          color: Colors.grey.shade300,
+                        ),
+                      ),
+                    ),
+                    Gap(20),
+                    CustomProfileField(
+                      label: 'Name',
+                      controller: name,
+                    ),
+                    Gap(20),
+                    CustomProfileField(
+                      label: 'Email',
+                      controller: email,
+                    ),
+                    Gap(20),
+                    CustomProfileField(
+                      label: 'Address',
+                      controller: address,
+                    ),
+                    Gap(20),
+
+                    Divider(color: Colors.white, thickness: 1),
+
+                    Gap(8),
+                    userModel?.visa == null ||
+                            userModel!.visa!.isEmpty
+                        ? CustomProfileField(
+                            label: 'Add Visa Card',
+                            controller: visa,
+                          )
+                        : ListTile(
+                            onTap: () {},
+                            contentPadding:
+                                const EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 5,
+                                ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(20),
+                            ),
+                            tileColor: Colors.white,
+                            title: const CustomText(
+                              text: 'Visa',
+                              fontSize: 20,
+                              color: Colors.black,
+                            ),
+                            subtitle: const CustomText(
+                              text: '**** **** **** 1234',
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                            leading: Image.asset(
+                              'assets/profile_visa.png',
+                              width: 80,
+                            ),
+                            trailing: CustomText(
+                              text: 'Default',
+                              color: Colors.black,
+                            ),
+                          ),
+                  ],
+                ),
+              ),
             ),
           ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 12.0,
-          vertical: 8,
-        ),
-        child: Column(
-          children: [
-            Center(
-              child: Container(
-                height: 120,
-                width: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(
+
+          bottomSheet: Container(
+            height: 90,
+            color: Colors.white,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                // Left Button: "Edit Profile"
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 18,
+                  ),
+                  decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Edit Profile",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      SizedBox(width: 8),
+                      Icon(Icons.edit, color: Colors.white),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 16), // Space between buttons
+                // Right Button: "Log out"
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 18,
+                  ),
+                  decoration: BoxDecoration(
                     color: Colors.white,
-                    width: 5,
+                    border: Border.all(
+                      color: AppColors.primary,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                  color: Colors.grey.shade300,
+                  child: Row(
+                    children: [
+                      Text(
+                        "Log out",
+                        style: TextStyle(
+                          color: AppColors.primary,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Icon(
+                        Icons.logout,
+                        color: AppColors.primary,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
-            Gap(20),
-            CustomProfileField(label: 'Name', controller: name),
-            Gap(20),
-            CustomProfileField(
-              label: 'Email',
-              controller: email,
-            ),
-            Gap(20),
-            CustomProfileField(
-              label: 'Address',
-              controller: address,
-            ),
-            Gap(20),
-
-            Divider(color: Colors.white, thickness: 1),
-
-            Gap(8),
-            ListTile(
-              onTap: () {},
-              contentPadding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 5,
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              tileColor: Colors.white,
-              title: const CustomText(
-                text: 'Visa',
-                fontSize: 20,
-                color: Colors.black,
-              ),
-              subtitle: const CustomText(
-                text: '**** **** **** 1234',
-                fontSize: 16,
-                color: Colors.black,
-              ),
-              leading: Image.asset(
-                'assets/profile_visa.png',
-                width: 80,
-              ),
-              trailing: CustomText(
-                text: 'Default',
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-      ),
-
-      bottomSheet: Container(
-        height: 90,
-        color: Colors.white,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            // Left Button: "Edit Profile"
-            Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 30,
-                vertical: 18,
-              ),
-              decoration: BoxDecoration(
-                color: AppColors.primary,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    "Edit Profile",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  SizedBox(width: 8),
-                  Icon(Icons.edit, color: Colors.white),
-                ],
-              ),
-            ),
-            SizedBox(width: 16), // Space between buttons
-            // Right Button: "Log out"
-            Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 30,
-                vertical: 18,
-              ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: AppColors.primary,
-                  width: 2,
-                ),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    "Log out",
-                    style: TextStyle(color: AppColors.primary),
-                  ),
-                  SizedBox(width: 8),
-                  Icon(Icons.logout, color: AppColors.primary),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
